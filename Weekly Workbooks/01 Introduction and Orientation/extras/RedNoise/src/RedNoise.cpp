13692077eeb29e0575d59c3c0ae17222f067f824
yumu987 <warmaradio@163.com>
2023-10-10 04:00:24 +0100

    Week 03

--- Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/src/RedNoise.cpp
+++ Weekly Workbooks/01 Introduction and Orientation/extras/RedNoise/src/RedNoise.cpp
@@ -33,50 +33,6 @@ std::vector<float> interpolateSingleFloats(float from, float to, int numberOfVal
 }
 
 std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 to, int numberOfValues) {
-	/*--------------------*/
-	// std::vector<glm::vec3> v;
-	// int num = numberOfValues - 1;
-	// int numtmp = numberOfValues - 2;
-	// v.push_back(from);
-	// for(int i = 0; i < numtmp; i++) {
-	// 	if(from.x > to.x) {
-	// 		float tmpFirst = from.x - to.x;
-	// 		float decreFirst = tmpFirst / num;
-	// 		from.x = from.x - decreFirst;
-	// 	} else if(from.x < to.x) {
-	// 		float tmpSecond = to.x - from.x;
-	// 		float increSecond = tmpSecond / num;
-	// 		from.x = from.x + increSecond;
-	// 	} else {
-	// 		from.x = from.x;
-	// 	}
-	// 	if(from.y > to.y) {
-	// 		float tmpThird = from.y - to.y;
-	// 		float decreThird = tmpThird / num;
-	// 		from.y = from.y - decreThird;
-	// 	} else if(from.y < to.y) {
-	// 		float tmpFourth = to.y - from.y;
-	// 		float increFourth = tmpFourth / num;
-	// 		from.y = from.y + increFourth;
-	// 	} else {
-	// 		from.y = from.y;
-	// 	}
-	// 	if(from.z > to.z) {
-	// 		float tmpFifth = from.z - to.z;
-	// 		float decreFifth = tmpFifth / num;
-	// 		from.z = from.z - decreFifth;
-	// 	} else if(from.z < to.z) {
-	// 		float tmpSixth = to.z - from.z;
-	// 		float increSixth = tmpSixth / num;
-	// 		from.z = from.z + increSixth;
-	// 	} else {
-	// 		from.z = from.z;
-	// 	}
-	// 	v.push_back(from);
-	// }
-	// v.push_back(to);
-	// return v;
-	/*--------------------*/
 	std::vector<glm::vec3> v;
 	std::vector<float> resultX;
 	std::vector<float> resultY;
@@ -95,7 +51,6 @@ std::vector<glm::vec3> interpolateThreeElementValues(glm::vec3 from, glm::vec3 t
 }
 
 CanvasTriangle generateThreeRandomVertices(CanvasTriangle triangle) {
-	// Random three vertices (points)
 	triangle.v0().x = rand() % WIDTH;
 	triangle.v0().y = rand() % HEIGHT;
 	triangle.v1().x = rand() % WIDTH;
@@ -106,7 +61,6 @@ CanvasTriangle generateThreeRandomVertices(CanvasTriangle triangle) {
 }
 
 Colour generateRandomColour(Colour c) {
-	// Random colour
 	c.red = rand() % 256;
 	c.green = rand() % 256;
 	c.blue = rand() % 256;
@@ -127,48 +81,205 @@ void drawLine(DrawingWindow &window, CanvasPoint from, CanvasPoint to, Colour c)
 	}
 }
 
-CanvasPoint flatLine(CanvasPoint from, CanvasPoint to, CanvasPoint extra) {
+CanvasPoint interpolateLine(CanvasPoint from, CanvasPoint to, CanvasPoint extra) {
 	// Line equation: y = mx + b
-	// m = yDiff / xDiff // m = (y2 - y1) / (x2 - x1)
-	// b = y - mx // Substitute one exact point (x1, y1) OR (x2, y2) to get constant b
-	// x = (y - b) / m // Use exact y value of point to get x
 	float xDiff = to.x - from.x;
 	float yDiff = to.y - from.y;
 	float m = yDiff / xDiff;
-	float b = to.y - (m * to.x); // Substitute one exact point to get constant b
-	extra.x = (extra.y - b) / m; // Substitute extraPoint's y
+	float b = to.y - (m * to.x);
+	if (xDiff == 0) { // Slope is 0
+		std::cout << "xDiff == 0 interpolateLine" << std::endl;
+		extra.x = to.x; // extra.x = from.x
+	} else if (yDiff == 0) { // Slope is 0
+		std::cout << "yDiff == 0 interpolateLine" << std::endl;
+		extra.x = to.x;
+	} else if (xDiff == 0 && yDiff == 0) { // Slope is both 0
+		std::cout << "xDiff == 0 && yDiff == 0 interpolateLine" << std::endl;
+		extra.x = to.x;
+	} else { // xDiff != 0 && yDiff != 0
+		extra.x = (extra.y - b) / m;
+	}
 	return extra;
 }
 
 void strokedTriangle(DrawingWindow &window, CanvasTriangle triangle, Colour c) {
-	// Random colour
 	c = generateRandomColour(c);
-	// Draw edge
 	drawLine(window, triangle.v0(), triangle.v1(), c);
 	drawLine(window, triangle.v1(), triangle.v2(), c);
 	drawLine(window, triangle.v2(), triangle.v0(), c);
 }
 
-void fillTopTriangle() {
-	// Fill top triangle by drawing lines
+void drawWhiteEdgeTriangle(DrawingWindow &window, CanvasTriangle triangle, Colour c) {
+	c.red = 255;
+	c.green = 255;
+	c.blue = 255;
+	drawLine(window, triangle.v0(), triangle.v1(), c);
+	drawLine(window, triangle.v1(), triangle.v2(), c);
+	drawLine(window, triangle.v2(), triangle.v0(), c);
 }
 
-void fillBottomTriangle() {
-	// Fill bottom triangle by drawing lines
+void fillTopTriangle(DrawingWindow &window, CanvasPoint top, CanvasPoint middle, CanvasPoint extra, Colour c) {
+	float xDiffF = middle.x - top.x;
+	float yDiffF = middle.y - top.y;
+	float mF = yDiffF / xDiffF;
+	float bF = middle.y - (mF * middle.x);
+	float xDiffS = extra.x - top.x;
+	float yDiffS = extra.y - top.y;
+	float mS = yDiffS / xDiffS;
+	float bS = extra.y - (mS * extra.x);
+	// Fill triangle
+	if (xDiffF == 0 && yDiffF == 0) { // middle = top
+		std::cout << "xDiffF == 0 && yDiffF == 0 fillTopTriangle" << std::endl;
+		std::cout << "pass" << std::endl;
+	} else if (xDiffS == 0 && yDiffS == 0) { // extra = top
+		std::cout << "xDiffS == 0 && yDiffS == 0 fillTopTriangle" << std::endl;
+		std::cout << "pass" << std::endl;
+	} else {
+		if (xDiffF == 0) { // middle.x = top.x
+			std::cout << "xDiffF == 0 fillTopTriangle" << std::endl;
+			CanvasPoint firstPoint;
+			CanvasPoint secondPoint;
+			firstPoint.x = top.x;
+			for (float i = top.y; i < middle.y; i++) {
+				firstPoint.y = i;
+				secondPoint.y = i;
+				secondPoint.x = (secondPoint.y - bS) / mS;
+				drawLine(window, firstPoint, secondPoint, c);
+			}
+		} else if (xDiffS == 0) { // extra.x = top.x
+			std::cout << "xDiffS == 0 fillTopTriangle" << std::endl;
+			CanvasPoint firstPoint;
+			CanvasPoint secondPoint;
+			secondPoint.x = top.x;
+			for (float i = top.y; i < middle.y; i++) {
+				firstPoint.y = i;
+				secondPoint.y = i;
+				firstPoint.x = (firstPoint.y - bF) / mF;
+				drawLine(window, firstPoint, secondPoint, c);
+			}
+		} else if (yDiffF == 0) { // middle.y = top.y
+			std::cout << "yDiffF == 0 fillTopTriangle" << std::endl;
+			CanvasPoint firstPoint;
+			CanvasPoint secondPoint;
+			firstPoint.y = top.y;
+			for (float i = top.x; i < middle.x; i++) {
+				firstPoint.x = i;
+				secondPoint.x = i;
+				secondPoint.y = (mS * secondPoint.x) + bS;
+				drawLine(window, firstPoint, secondPoint, c);
+			}
+		} else if (yDiffS == 0) { // extra.y = top.y
+			std::cout << "yDiffS == 0 fillTopTriangle" << std::endl;
+			CanvasPoint firstPoint;
+			CanvasPoint secondPoint;
+			secondPoint.y = top.y;
+			for (float i = top.x; i < middle.x; i++) {
+				firstPoint.x = i;
+				secondPoint.x = i;
+				firstPoint.y = (mF * firstPoint.x) + bF;
+				drawLine(window, firstPoint, secondPoint, c);
+			}
+		} else {
+			CanvasPoint firstPoint;
+			CanvasPoint secondPoint;
+			for (float i = top.y; i < middle.y; i++) {
+				firstPoint.y = i;
+				secondPoint.y = i;
+				firstPoint.x = (firstPoint.y - bF) / mF;
+				secondPoint.x = (secondPoint.y - bS) / mS;
+				drawLine(window, firstPoint, secondPoint, c);
+			}
+		}
+	}
+}
+
+void fillBottomTriangle(DrawingWindow &window, CanvasPoint bottom, CanvasPoint middle, CanvasPoint extra, Colour c) {
+	float xDiffF = bottom.x - middle.x;
+	float yDiffF = bottom.y - middle.y;
+	float mF = yDiffF / xDiffF;
+	float bF = middle.y - (mF * middle.x);
+	float xDiffS = bottom.x - extra.x;
+	float yDiffS = bottom.y - extra.y;
+	float mS = yDiffS / xDiffS;
+	float bS = extra.y - (mS * extra.x);
+	// Fill triangle
+	if (xDiffF == 0 && yDiffF == 0) { // bottom = middle
+		std::cout << "xDiffF == 0 && yDiffF == 0 fillBottomTriangle" << std::endl;
+		std::cout << "pass" << std::endl;
+	} else if (xDiffS == 0 && yDiffS == 0) { // bottom = extra
+		std::cout << "xDiffS == 0 && yDiffS == 0 fillBottomTriangle" << std::endl;
+		std::cout << "pass" << std::endl;
+	} else {
+		if (xDiffF == 0) { // bottom.x = middle.x
+			std::cout << "xDiffF == 0 fillBottomTriangle" << std::endl;
+			CanvasPoint firstPoint;
+			CanvasPoint secondPoint;
+			firstPoint.x = bottom.x;
+			for (float i = middle.y; i < bottom.y; i++) {
+				firstPoint.y = i;
+				secondPoint.y = i;
+				secondPoint.x = (secondPoint.y - bS) / mS;
+				drawLine(window, firstPoint, secondPoint, c);
+			}
+		} else if (xDiffS == 0) { // bottom.x = extra.x
+			std::cout << "xDiffS == 0 fillBottomTriangle" << std::endl;
+			CanvasPoint firstPoint;
+			CanvasPoint secondPoint;
+			secondPoint.x = bottom.x;
+			for (float i = middle.y; i < bottom.y; i++) {
+				firstPoint.y = i;
+				secondPoint.y = i;
+				firstPoint.x = (firstPoint.y - bF) / mF;
+				drawLine(window, firstPoint, secondPoint, c);
+			}
+		} else if (yDiffF == 0) { // bottom.y = middle.y
+			std::cout << "yDiffF == 0 fillBottomTriangle" << std::endl;
+			CanvasPoint firstPoint;
+			CanvasPoint secondPoint;
+			firstPoint.y = bottom.y;
+			for (float i = middle.x; i < bottom.x; i++) {
+				firstPoint.x = i;
+				secondPoint.x = i;
+				secondPoint.y = (mS * secondPoint.x) + bS;
+				drawLine(window, firstPoint, secondPoint, c);
+			}
+		} else if (yDiffS == 0) { // bottom.y = extra.y
+			std::cout << "yDiffS == 0 fillBottomTriangle" << std::endl;
+			CanvasPoint firstPoint;
+			CanvasPoint secondPoint;
+			secondPoint.y = bottom.y;
+			for (float i = middle.x; i < bottom.x; i++) {
+				firstPoint.x = i;
+				secondPoint.x = i;
+				firstPoint.y = (mF * firstPoint.x) + bF;
+				drawLine(window, firstPoint, secondPoint, c);
+			}
+		} else {
+			CanvasPoint firstPoint;
+			CanvasPoint secondPoint;
+			for (float i = middle.y; i < bottom.y; i++) {
+				firstPoint.y = i;
+				secondPoint.y = i;
+				firstPoint.x = (firstPoint.y - bF) / mF;
+				secondPoint.x = (secondPoint.y - bS) / mS;
+				drawLine(window, firstPoint, secondPoint, c);
+			}
+		}
+	}
 }
 
 void filledTriangle(DrawingWindow &window, CanvasTriangle triangle, Colour c) {
+	// Draw white edge
+	drawWhiteEdgeTriangle(window, triangle, c);
 	// Random colour
 	c = generateRandomColour(c);
-	// Draw edge
-	drawLine(window, triangle.v0(), triangle.v1(), c);
-	drawLine(window, triangle.v1(), triangle.v2(), c);
-	drawLine(window, triangle.v2(), triangle.v0(), c);
+	// Points
 	CanvasPoint topPoint; // Close to the top edge
 	CanvasPoint middlePoint; // Close to the middle edge
 	CanvasPoint bottomPoint; // Close to the bottom edge
-	CanvasPoint extraPoint; // Extra point has the same y-coordinate with middle point
-	// Sort vertices by vertical position (top to bottom)
+	CanvasPoint extraPoint; // It has the same y coordinate with middlePoint
+	// Sort vertices by vertical position (from top to bottom)
+	// If y != y && x != x...
 	if (triangle.v0().y > triangle.v1().y && triangle.v0().y > triangle.v2().y) {
 		bottomPoint = triangle.v0();
 		if (triangle.v1().y > triangle.v2().y) {
@@ -197,73 +308,72 @@ void filledTriangle(DrawingWindow &window, CanvasTriangle triangle, Colour c) {
 			topPoint = triangle.v0();
 		}
 	}
+	// If y == y && x == x...
+	if (triangle.v0().y == triangle.v1().y && triangle.v0().x == triangle.v2().x) {
+		topPoint = triangle.v2();
+		middlePoint = triangle.v1();
+		bottomPoint = triangle.v0();
+		extraPoint = bottomPoint;
+	} else if (triangle.v1().y == triangle.v2().y && triangle.v1().x == triangle.v0().x) {
+		topPoint = triangle.v0();
+		middlePoint = triangle.v2();
+		bottomPoint = triangle.v1();
+		extraPoint = bottomPoint;
+	} else if (triangle.v2().y == triangle.v1().y && triangle.v2().x == triangle.v0().x) {
+		topPoint = triangle.v0();
+		middlePoint = triangle.v1();
+		bottomPoint = triangle.v2();
+		extraPoint = bottomPoint;
+	} else if (triangle.v0().y == triangle.v2().y && triangle.v0().x == triangle.v1().x) {
+		topPoint = triangle.v1();
+		middlePoint = triangle.v2();
+		bottomPoint = triangle.v0();
+		extraPoint = bottomPoint;
+	} else if (triangle.v1().y == triangle.v0().y && triangle.v1().x == triangle.v2().x) {
+		topPoint = triangle.v2();
+		middlePoint = triangle.v0();
+		bottomPoint = triangle.v1();
+		extraPoint = bottomPoint;
+	} else if (triangle.v2().y == triangle.v0().y && triangle.v2().x == triangle.v1().x) {
+		topPoint = triangle.v1();
+		middlePoint = triangle.v0();
+		bottomPoint = triangle.v2();
+		extraPoint = bottomPoint;
+	} else {
+		// If y == y && x != x...
+		if (triangle.v0().y == triangle.v1().y) {
+			topPoint = triangle.v2();
+			middlePoint = triangle.v0();
+			bottomPoint = triangle.v1();
+			extraPoint = bottomPoint;
+		} else if (triangle.v1().y == triangle.v2().y) {
+			topPoint = triangle.v0();
+			middlePoint = triangle.v1();
+			bottomPoint = triangle.v2();
+			extraPoint = bottomPoint;
+		} else if (triangle.v2().y == triangle.v0().y) {
+			topPoint = triangle.v1();
+			middlePoint = triangle.v2();
+			bottomPoint = triangle.v0();
+			extraPoint = bottomPoint;
+		}
+		// If y != y && x == x...
+		// They are handled by previous sort vertices methods
+	}
 	// Divide triangle into 2 "flat-bottomed" triangles
 	extraPoint.y = middlePoint.y;
-	extraPoint = flatLine(topPoint, bottomPoint, extraPoint);
+	extraPoint = interpolateLine(topPoint, bottomPoint, extraPoint);
 	// Draw a line to divide triangle
 	drawLine(window, middlePoint, extraPoint, c);
-	// Fill top triangle
-	// ......
-	// Fill bottom triangle
-	// ......
+	// Fill triangle
+	fillTopTriangle(window, topPoint, middlePoint, extraPoint, c);
+	fillBottomTriangle(window, bottomPoint, middlePoint, extraPoint, c);
 }
 
 void draw(DrawingWindow &window) {
 	window.clearPixels();
-	/*--------------------*/
-	// Black & White
-	// float startColour = 255; // White colour
-	// float endColour = 0; // Black colour
-	/*--------------------*/
-	// Colour
-	// glm::vec3 topLeft(255, 0, 0);        // red 
-	// glm::vec3 topRight(0, 0, 255);       // blue 
-	// glm::vec3 bottomRight(0, 255, 0);    // green 
-	// glm::vec3 bottomLeft(255, 255, 0);   // yellow
-	// std::vector<glm::vec3> rightColumn;
-	// std::vector<glm::vec3> leftColumn;
-	// rightColumn = interpolateThreeElementValues(topLeft, bottomLeft, HEIGHT);
-	// leftColumn = interpolateThreeElementValues(topRight, bottomRight, HEIGHT);
-	/*--------------------*/
 	for (size_t y = 0; y < window.height; y++) {
-		/*--------------------*/
-		// Colour
-		// std::vector<glm::vec3> rowVector;
-		// rowVector = interpolateThreeElementValues(rightColumn[y], leftColumn[y], WIDTH);
-		/*--------------------*/
 		for (size_t x = 0; x < window.width; x++) {
-			/*--------------------*/
-			// RedNoise
-			// float red = rand() % 256;
-			// float green = 0.0;
-			// float blue = 0.0;
-			// uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-			// window.setPixelColour(x, y, colour);
-			/*--------------------*/
-			// Greyscale
-			// float red = (startColour + (endColour - startColour) * x / WIDTH);
-			// float green = (startColour + (endColour - startColour) * x / WIDTH);
-			// float blue = (startColour + (endColour - startColour) * x / WIDTH);
-			// uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-			// window.setPixelColour(x, y, colour);
-			/*--------------------*/
-			// Greyscale
-			// std::vector<float> resultFor;
-			// resultFor = interpolateSingleFloats(255, 0, WIDTH); // split from white to black for a whole window
-			// // iterate from strat index to end index
-			// float red = resultFor[x];
-			// float green = resultFor[x];
-			// float blue = resultFor[x];
-			// uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-			// window.setPixelColour(x, y, colour);
-			/*--------------------*/
-			// Colour
-			// float red = rowVector[x].x;
-			// float green = rowVector[x].y;
-			// float blue = rowVector[x].z;
-			// uint32_t colour = (255 << 24) + (int(red) << 16) + (int(green) << 8) + int(blue);
-			// window.setPixelColour(x, y, colour);
-			/*--------------------*/
 			// Test Black Background
 			float red = 0.0;
 			float green = 0.0;
@@ -281,13 +391,13 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
 		else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
 		else if (event.key.keysym.sym == SDLK_u) { // u: Draw random stroked (unfilled) triangle
-			std::cout << "u" << "/" << "U" << std::endl;
+			std::cout << "u" << "/" << "U" << ":" << "Stroked triangle" << std::endl;
 			CanvasTriangle triangle;
 			Colour c;
 			triangle = generateThreeRandomVertices(triangle);
 			strokedTriangle(window, triangle, c);
 		} else if (event.key.keysym.sym == SDLK_f) { // f: Draw random filled triangle
-			std::cout << "f" << "/" << "F" << std::endl;
+			std::cout << "f" << "/" << "F" << ":" << "Filled triangle" << std::endl;
 			CanvasTriangle triangle;
 			Colour c;
 			triangle = generateThreeRandomVertices(triangle);
@@ -302,10 +412,7 @@ void handleEvent(SDL_Event event, DrawingWindow &window) {
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
-	draw(window); // Fixed black background
-	/*--------------------*/
-	// std::cout << std::endl; // Debug copy
-	/*--------------------*/
+	draw(window);
 	while (true) {
 		// We MUST poll for events - otherwise the window will freeze !
 		if (window.pollForInputEvents(event)) handleEvent(event, window);
